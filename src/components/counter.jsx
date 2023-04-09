d74fa6f1b1e2ea45e3845e2ed6bb32646c9927fd
Juan Carlos Avila <juanvs23@gmail.com>
2023-04-09 12:52:36 -0400

    react completed

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,10 +1,12 @@
-import { useState } from "react"
+import { useState } from 'react';
 
-export default function Counter(){
-    const [counter,setCounter] = useState(0)
-    return(
-        <div className="counter">
-            <button onClick={e=>setCounter(counter=> counter + 1)} id="count">Click {counter} times</button>
-       </div>
-    )
+export default function Counter() {
+  const [counter, setCounter] = useState(0);
+  return (
+    <div className="counter">
+      <button onClick={e => setCounter(counter => counter + 1)} id="count">
+        Click {counter} times
+      </button>
+    </div>
+  );
 }
--- vite.config.js
+++ vite.config.js
@@ -2,43 +2,43 @@
 import { defineConfig } from 'vite';
 import { createHtmlPlugin } from 'vite-plugin-html';
 import path from 'path';
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
- base: '/',
- root: path.join(__dirname, '/src'),
- build: {
-  input: path.join(__dirname, '/src'),
-  outDir: `${__dirname}/dist`,
-  sourcemap: true,
-  rollupOptions: {
-   input: path.join(__dirname, '/src/app.js'),
-   output: {
-    assetFileNames: 'assets/css/[name].[ext]',
-    entryFileNames: 'assets/js/[name].js',
-   },
-  },
-  manifest: true,
- },
- emitIndex: true,
- plugins: [
-   react(),
-  // https://github.com/vbenjs/vite-plugin-html
-  createHtmlPlugin({
-   minify: false,
-   pages: [
-    {
-     path: '/',
-     filename: 'index.html',
-     template: './index.html',
-     injectOptions: {
-      data: {
-       title: 'Vanilla bundle',
-       // injectScript: `<script src="./inject.js"></script>`,
+  base: '/',
+  root: path.join(__dirname, '/src'),
+  build: {
+    input: path.join(__dirname, '/src'),
+    outDir: `${__dirname}/dist`,
+    sourcemap: true,
+    rollupOptions: {
+      input: path.join(__dirname, '/src/app.js'),
+      output: {
+        assetFileNames: 'assets/css/[name].[ext]',
+        entryFileNames: 'assets/js/[name].js',
       },
-     },
     },
-   ],
-  }),
- ],
+    manifest: true,
+  },
+  emitIndex: true,
+  plugins: [
+    react(),
+    // https://github.com/vbenjs/vite-plugin-html
+    createHtmlPlugin({
+      minify: false,
+      pages: [
+        {
+          path: '/',
+          filename: 'index.html',
+          template: './index.html',
+          injectOptions: {
+            data: {
+              title: 'Vanilla bundle',
+              // injectScript: `<script src="./inject.js"></script>`,
+            },
+          },
+        },
+      ],
+    }),
+  ],
 });
